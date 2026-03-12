e1bb7b011fcfa138ee327169c9503d33210ddb34
satya-500 <satyabrata.swain@500apps.com>
2026-03-12 10:23:56 +0530

    refactor: update environment variables and AgentPanel configuration for improved user interaction and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,32 @@ import { AgentPanel } from '@agilecrm/500ux-core'
 function App() {
   return (
     <div className='w-full h-full' >
-        <AgentPanel
-          title={import.meta.env.VITE_AGENT_TITLE || ''}
-          subtitle={import.meta.env.VITE_AGENT_SUBTITLE || ''}
-          logo={
-            <img
-              src="https://r-one.co.in/wp-content/uploads/2025/10/rone-logo-1.png"
-              alt="Robot Logo"
-              style={{
-                width: 100,
-                height: 36,
-                borderRadius: 10,
-                objectFit: 'contain',
-              }}
-            />
+      <AgentPanel
+        title={import.meta.env.VITE_AGENT_TITLE || ''}
+        subtitle={import.meta.env.VITE_AGENT_SUBTITLE || ''}
+        logo={
+          <img
+            src="https://r-one.co.in/wp-content/uploads/2025/10/rone-logo-1.png"
+            alt="Robot Logo"
+            style={{
+              width: 100,
+              height: 36,
+              borderRadius: 10,
+              objectFit: 'contain',
+            }}
+          />
+        }
+        context={{
+          appId: '69affabefcbda71b0de2cab7',
+          metadata: {
+            prompt: "You are R-ONE's customer-facing agent, designed to assist visitors with inquiries related to our real estate projects in Hyderabad, including residential skyscrapers and mixed-use developments. Your tone should be friendly and professional, reflecting our commitment to innovative design and community-centric living. Provide accurate information based on the content available on our website and your training data. If you encounter questions beyond your knowledge, kindly redirect users to our contact support for further assistance.",
+            training_id: '0b2919bf-070a-46d5-8e08-63493487f903',
           }
-          context={{
-            appId: import.meta.env.VITE_AGENT_APP_ID || '',
-            metadata: {
-              jug_prompt: import.meta.env.VITE_AGENT_JUG_PROMPT || ''
-            }
-          }}
-          placeholder="Ask the agent anything..."
-          appLabel={import.meta.env.VITE_AGENT_APP_LABEL || ''}
-          debug={false}
-        />
+        }}
+        placeholder="How can I help you today?"
+        appLabel={import.meta.env.VITE_AGENT_APP_LABEL || ''}
+        debug={false}
+      />
     </div>
   )
 }
